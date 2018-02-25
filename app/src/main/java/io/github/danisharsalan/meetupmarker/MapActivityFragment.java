c2ff119cda747d643cb88d5c2b3e58cc2b255e80
aihoque2 <aihoque2@illinois.edu>
2018-02-25 07:10:47 -0600

    security!

--- app/src/main/java/io/github/danisharsalan/meetupmarker/MapActivityFragment.java
+++ app/src/main/java/io/github/danisharsalan/meetupmarker/MapActivityFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
